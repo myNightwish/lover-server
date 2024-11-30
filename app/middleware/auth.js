e868e08b54b5ea43af0a821c0d9218166d854450
zhoumaoling <zhoumaoling@baidu.com>
2024-12-01 01:08:11 +0800

    数据库

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -5,7 +5,7 @@ module.exports = () => {
     const authorizationHeader = ctx.headers.authorization;
 
     if (!authorizationHeader) {
-      ctx.throw(401, 'Authorization header missing');
+      ctx.throw(401, { message: 'Token expired', isTokenExpired: true });
     }
     const token = authorizationHeader.split(' ')[1]; // 获取 Bearer 后的 token
 
@@ -13,24 +13,24 @@ module.exports = () => {
       ctx.throw(401, 'Authentication token is missing');
     }
 
-    // try {
+    try {
     // 验证 token 并解析用户信息
-    const user = await ctx.service.auth.verifyToken(token); // 使用 auth 服务解析 token
-    if (!user) {
-      ctx.throw(401, 'Invalid token----0');
+      const user = await ctx.service.auth.verifyToken(token); // 使用 auth 服务解析 token
+      if (!user) {
+        ctx.throw(401, 'Invalid token----0');
+      }
+
+      // 将用户信息绑定到 ctx 对象上
+      ctx.user = user;
+
+      // 继续执行后续逻辑
+      await next();
+    } catch (error) {
+      if (error.name === 'TokenExpiredError') {
+        ctx.throw(401, { message: 'Token expired', isTokenExpired: true });
+      } else {
+        ctx.throw(401, 'Invalid token-----1');
+      }
     }
-
-    // 将用户信息绑定到 ctx 对象上
-    ctx.user = user;
-
-    // 继续执行后续逻辑
-    await next();
-    // } catch (error) {
-    //   if (error.name === 'TokenExpiredError') {
-    //     ctx.throw(401, 'Token expired');
-    //   } else {
-    //     ctx.throw(401, 'Invalid token-----1');
-    //   }
-    // }
   };
 };
